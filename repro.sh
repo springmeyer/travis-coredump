99d2e5f305ab39400959b18a7e8797c1a0900581
Buck Golemon <buck@yelp.com>
2014-11-10 10:28:51 -0800

    enable debug symbols for coveragepy

--- repro.sh
+++ repro.sh
@@ -6,6 +6,7 @@ mkdir clean
 cd clean/
 virtualenv tmpvenv
 
+export CFLAGS='-g'  # enable debug symbols for extensions built by pip
 ./tmpvenv/bin/pip install --upgrade pip wheel coverage
 ./tmpvenv/bin/pip wheel coverage
 ./tmpvenv/bin/python -m coverage.__main__ run -m pip install --no-index --ignore-installed --find-links wheelhouse/ coverage
