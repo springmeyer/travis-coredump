150ecf03f743209751b2a252392113b0f7aa6f28
Dane Springmeyer <dane@mapbox.com>
2015-07-14 08:04:15 -0700

    try enabling winsdk

--- test.bat
+++ test.bat
@@ -21,11 +21,12 @@ SET PATH=C:\Program Files (x86)\Microsoft Visual Studio %msvs_toolset%.0\VC\bin;
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 :: note: will override %PLATFORM% to upper case
-ECHO "enabling VC env"
+ECHO "enabling VC env %platform%"
 if %platform% == x64 CALL "C:\Program Files (x86)\Microsoft Visual Studio %msvs_toolset%.0\VC\vcvarsall.bat" amd64
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 if %platform% == x86 CALL "C:\Program Files (x86)\Microsoft Visual Studio %msvs_toolset%.0\VC\vcvarsall.bat" amd64_x86
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+ECHO "VC env %platform% enabled"
 
 ECHO "compiling test program"
 cl /MD /Zi /Fmight_crash.pdb /nologo /EHsc /D NDEBUG might_crash.cpp
@@ -40,11 +41,17 @@ ECHO "running test program again"
 set CRASH_PLEASE=1
 .\might_crash
 
+SET WIN_SDK_ROOT=C:\Program Files\Microsoft SDKs\Windows
+set WINDOWS_SDK_VERSION=v7.1
+"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
+"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /%platform% /release
+
 IF EXIST %LOCALAPPDATA%\CrashDumps (
   ECHO CrashDumps found
   dir %LOCALAPPDATA%\CrashDumps
   ntsd
   cdb
+
 )
 
 GOTO DONE
