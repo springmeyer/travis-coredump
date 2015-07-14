fc2986d722c6ceec155457848809dfcdf67795ee
Dane Springmeyer <dane@mapbox.com>
2015-07-14 08:19:51 -0700

    use call

--- test.bat
+++ test.bat
@@ -43,8 +43,10 @@ set CRASH_PLEASE=1
 
 SET WIN_SDK_ROOT=C:\Program Files\Microsoft SDKs\Windows
 set WINDOWS_SDK_VERSION=v7.1
-"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
-"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /%platform% /release
+ECHO "Running WindowsSdkVer.exe"
+call "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
+ECHO "Running SetEnv.cmd"
+call "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /%platform% /release
 
 IF EXIST %LOCALAPPDATA%\CrashDumps (
   ECHO CrashDumps found
