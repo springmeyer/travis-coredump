8c000ad302607e442999b795060e1551fd412f5a
Dane Springmeyer <dane@mapbox.com>
2016-08-08 16:29:41 -0700

    look at hard limits

--- ci-build.sh
+++ ci-build.sh
@@ -4,17 +4,20 @@ set -eu
 set -o pipefail
 
 ulimit -c
+sudo bash -c "ulimit -c unlimited"
+ulimit -c unlimited -H
 ulimit -c unlimited -S
 ulimit -c
 ulimit -a -S
+ulimit -a -H
 cat /proc/sys/kernel/core_pattern || true
 sysctl kernel.core_pattern
-sudo service apport stop || true
+sudo service apport restart || true
 echo '#!/usr/bin/env bash' > apport
 echo '' >> apport
 echo "echo yes >> $(pwd)/core" >> apport
 chmod +x ./apport
-sudo cp apport /usr/share/apport/apport
+#sudo cp apport /usr/share/apport/apport
 sudo bash -c "chmod +x /usr/share/apport/apport"
 sudo bash -c "cat /proc/sys/kernel/core_pattern" || true
 # the below hits: 'bash: /proc/sys/kernel/core_pattern: Permission denied'
@@ -29,7 +32,7 @@ make
 # `|| true` to ensure that travis continues past the crash
 RESULT=$(./test > /dev/null)$? || true
 ls
-ls /var/crash || true
+ls /var/crash/ || true
 cat /var/log/apport.log || true
 if [[ ${RESULT} == 0 ]]; then echo "\\o/ our test worked without problems"; else echo "ruhroh test returned an errorcode of $RESULT"; fi;
 # If the program returned an error code, now we check for a
